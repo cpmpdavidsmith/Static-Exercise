15e9bab26e49cea7123a4287e6503f4a0461bf92
David Smith <cpmpdavidsmith@icloud.com>
2023-02-16 12:56:16 -0500

    static exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celsius = TempConverter.FahrenheitToCelsius(69);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine($"{celsius}");
+            Console.WriteLine($"{fahrenheit}");
         }
     }
 }
